1c4cc471fe80f665e116c51097bfa18178cdab77
YuechengLiu <liuy150617@163.com>
2022-04-19 10:50:30 +0800

    map

--- ILSwiss/run_scripts/visualize_smarts_history.py
+++ ILSwiss/run_scripts/visualize_smarts_history.py
@@ -1,14 +1,7 @@
-import yaml
-import argparse
-import joblib
 import os
 import sys
-import gym
 import inspect
-import pickle
-import numpy as np
 import signal
-import random
 from subprocess import Popen
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -16,27 +9,31 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 print(sys.path)
 
-from rlkit.envs import get_env
 from smarts.core.smarts import SMARTS
-import rlkit.torch.utils.pytorch_util as ptu
-from rlkit.launchers.launcher_util import set_seed
-from rlkit.envs.wrappers import ProxyEnv, NormalizedBoxActEnv, ObsScaledEnv
-from rlkit.torch.common.policies import MakeDeterministic
 from smarts.core.scenario import Scenario
 from envision.client import Client as Envision
-from smarts_imitation import ScenarioZoo
+#from smarts_imitation import ScenarioZoo
 
 
-def experiment():
-    scenario_name = "ngsim_i80"
-    scenarios = ScenarioZoo.get_scenario(scenario_name)
+def experiment(scenario_path):
+    traffic_name = "us101_0750-0805"
+    scenario_name = "ngsim_us101"
+
+    #scenario_path = ScenarioZoo.get_scenario(scenario_name)
+
+    scenarios = "./smarts-imitation/scenarios/ngsim_us101"
     scenario_iterator = Scenario.scenario_variations(
-        [scenarios], list([]), shuffle_scenarios=False, circular=False
+        [scenario_path], list([]), shuffle_scenarios=False, circular=False
     )  # scenarios with different traffic histories.
-    scenario = next(scenario_iterator)
+    scenario = None
+    for _scenario in scenario_iterator:
+        if _scenario._traffic_history.name == traffic_name:
+            scenario = _scenario
+            break
+    assert scenario is not None
 
     envision_client = Envision(
-        sim_name="ngsim_us101",
+        sim_name=f"{scenario_name}_{traffic_name}",
         output_dir="record_data_replay_path",
         headless=False,
     )
@@ -55,16 +52,16 @@ def experiment():
 
 
 if __name__ == "__main__":
+    scenario_path = "./smarts-imitation/scenarios/ngsim_us101"
     # Arguments
-
     envision_proc = Popen(
-        f"scl envision start -s {ScenarioZoo.get_scenario('ngsim_i80')} -p 8081",
+        f"scl envision start -s {scenario_path} -p 8081",
         shell=True,
         preexec_fn=os.setsid,
     )
 
     try:
-        experiment()
+        experiment(scenario_path)
     except Exception as e:
         os.killpg(os.getpgid(envision_proc.pid), signal.SIGTERM)
         envision_proc.wait()
