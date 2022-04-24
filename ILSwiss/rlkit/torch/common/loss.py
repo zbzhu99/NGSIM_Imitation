385d394dfe8746d0bd9b5e1260326ff3f1d3f709
YuechengLiu <liuy150617@163.com>
2022-04-24 08:35:05 +0800

    focalbce bug

--- ILSwiss/rlkit/torch/common/loss.py
+++ ILSwiss/rlkit/torch/common/loss.py
@@ -9,12 +9,13 @@ class BCEFocalLoss(nn.Module):
         self.gamma = gamma
 
     def forward(self, inputs, targets):
-        targets = targets.long()
+        targets = targets.long().squeeze()
         prob_1 = torch.sigmoid(inputs)
         prob_0 = 1.0 - prob_1
         soft_inputs = torch.concat([prob_0, prob_1], dim=-1)
         target_one_hot = F.one_hot(targets, num_classes=2)
         weight = torch.pow(1.0 - soft_inputs, self.gamma)
         focal = -weight * torch.log(soft_inputs)
+        assert target_one_hot.shape == focal.shape
         loss = torch.sum(target_one_hot * focal, dim=-1).mean()
         return loss
