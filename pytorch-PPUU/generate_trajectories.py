69141e91d700d9a258ae6ef6ccafb9803008d44f
zbzhu99 <zbzhu99@icloud.com>
2023-03-14 05:38:02 +0000

    Update gen_traj script

--- pytorch-PPUU/generate_trajectories.py
+++ pytorch-PPUU/generate_trajectories.py
@@ -14,7 +14,7 @@ parser.add_argument("-traffic_rate", type=int, default=15)
 parser.add_argument("-state_image", type=int, default=1)
 parser.add_argument("-save_images", type=int, default=0)
 parser.add_argument("-store", type=int, default=1)
-parser.add_argument("-data_dir", type=str, default="traffic-data/state-action-cost-v2/")
+parser.add_argument("-data_dir", type=str, default="traffic-data/state-action-cost/")
 parser.add_argument("-fps", type=int, default=30)
 parser.add_argument("-time_slot", type=int, default=0)
 parser.add_argument(
@@ -41,6 +41,8 @@ kwargs = dict(
     traffic_rate=opt.traffic_rate,
     data_dir=opt.data_dir,
     delta_t=opt.delta_t,
+    normalise_state=False,
+    normalise_action=False,
 )
 
 register(id="Traffic-v0", entry_point="traffic_gym:Simulator", kwargs=kwargs)
@@ -79,7 +81,7 @@ env = gym.make(env_names[opt.map])
 env.reset(frame=0, time_slot=opt.time_slot)
 done = False
 while not done:
-    observation, reward, done, info = env.step()
+    observation, reward, done, info = env.step(np.zeros((2,)))
     env.render()
 
 print(f"Data generation for <{opt.map}, time slot {opt.time_slot}> completed")
